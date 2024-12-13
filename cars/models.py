4cda189f0079b63d027fe013ad6f854b8ef0dc1e
José Vitor <jose.vitor@thomasicamargo.local>
2024-12-13 11:28:53 -0300

    Templates e Consulta ORM Django

--- cars/models.py
+++ cars/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to='brand/', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
+def cars_view(request):
+    search = request.GET.get('search')
+
+    if (search):
+        cars = Car.objects.filter(model__icontains=search)
+    else:
+        cars = Car.objects.all()
+
+    cars = cars.order_by('model')
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars})
