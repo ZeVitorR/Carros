9637a790980c1692fa286f3f3d078e9d28b01d33
José Vitor <jose.vitor@thomasicamargo.local>
2024-12-16 18:29:53 -0300

    autenticação

--- cars/forms.py
+++ cars/forms.py
@@ -17,3 +17,5 @@ class CarModelForm(forms.ModelForm):
         if factory_year < 1975:
             self.add_error('factory_year','Não é possivel cadastrar carros fabricados antes de 1975')
         return factory_year
+    
+    
