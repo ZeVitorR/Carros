734365b537ed9fa005399bf61b44ff0e5813fcf5
José Vitor <jose.vitor@thomasicamargo.local>
2024-12-17 18:26:12 -0300

    Inserindo validação na view se o user está logado

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,8 @@
 from .models import Car
 from .forms import CarModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 class CarsListView(ListView):
@@ -15,6 +17,7 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
@@ -25,6 +28,7 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -34,6 +38,7 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
