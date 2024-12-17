1dacf4e27f89a56ac8a7c9be9352eafb2babe14a
José Vitor <jose.vitor@thomasicamargo.local>
2024-12-17 17:37:14 -0300

    Inserindo o UpdateView e DeleteView

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
 from .models import Car
 from .forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 class CarsListView(ListView):
     model = Car
@@ -16,43 +15,26 @@ class CarsListView(ListView):
             cars = cars.filter(model__icontains=search)
         return cars
 
-def new_cars_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(
-        request,
-        'new_car.html',
-        {'new_car_form': new_car_form}
-    )
-
-class NewCarView(View):
-
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(
-            request,
-            'new_car.html',
-            {'new_car_form': new_car_form}
-        )
-   
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(
-            request,
-            'new_car.html',
-            {'new_car_form': new_car_form}
-        )
-
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
+    success_url = '/cars/'
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
     success_url = '/cars/'
