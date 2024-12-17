93fc545f46fdf5eb6349b41dc62e20449a3fae96
José Vitor <jose.vitor@thomasicamargo.local>
2024-12-17 15:50:08 -0300

    Refatornado Views para Classes Based View: Utilizado as views genéricas

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Car
 from .forms import CarModelForm
+from django.views import View
 
-# Create your views here.
 def cars_view(request):
     search = request.GET.get('search')
 
@@ -18,6 +18,20 @@ def cars_view(request):
         'cars.html',
         {'cars': cars})
 
+class CarsView(View):
+
+    def get(self, request):
+        search = request.GET.get('search')
+        if (search):
+            cars = Car.objects.filter(model__icontains=search)
+        else:
+            cars = Car.objects.all()
+        cars = cars.order_by('model')
+        return render(
+            request,
+            'cars.html',
+            {'cars': cars})
+    
 def new_cars_view(request):
     if request.method == 'POST':
         new_car_form = CarModelForm(request.POST, request.FILES)
@@ -31,3 +45,24 @@ def new_cars_view(request):
         'new_car.html',
         {'new_car_form': new_car_form}
     )
+
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(
+            request,
+            'new_car.html',
+            {'new_car_form': new_car_form}
+        )
+   
+    def post(self, request):
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        return render(
+            request,
+            'new_car.html',
+            {'new_car_form': new_car_form}
+        )
