78bd0fbb2a6cac2aea876f22f40e642ff45d3bf2
José Vitor <rodriguesze6@gmail.com>
2024-12-19 18:05:04 -0300

    Utilizando a Open ia

--- cars/models.py
+++ cars/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-#Criando o model de marcas de carro
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to='brand/', blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
-# Criando o model de informação do carro
+
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
@@ -19,7 +18,19 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
 
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
